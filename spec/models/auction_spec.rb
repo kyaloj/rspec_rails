4c53caabac03ec8760de481b3b66a64255fea78a
Justus <mj.kyalo@gmail.com>
2017-01-18 00:16:34 +0300

    damn factories...

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
+require 'factory_girl_rails'
 
 RSpec.describe Auction, type: :model do
-  it 'is valid with valid attributes'
-  it 'is not valid without a title'
+  it 'has a valid factory' do
+    FactoryGirl.create(:Auction).should be_valid
+  end
+
+  it 'is valid with valid attributes' do
+    expect(Auction.new).to be_valid
+  end
+
+  it 'is not valid without a title' do
+  end
   it 'is not valid without a description'
   it 'is not valid without a start_date'
   it 'is not valid without a end_date'
